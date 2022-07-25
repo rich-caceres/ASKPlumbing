0d9175cb039b5bd8a1db285ba01d8754fd9b72b0
rich-caceres <richard.caceres22590@outlook.com>
2022-07-25 17:34:25 -0400

    initial commit for the ask plumbing app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ASKPlumbing';
+  title = 'A.S.K. Plumbing & Heating';
+  services = ['Drainage Services', 'Leaky Pipes Repair', 'Water Heater Repair', 'Sewer Stoppage Cleaning', 'Yearly Plumbing Inspection', 'Plumbing Diagnostics'];
+  businessStatement = 'No Job is too big or too small, call us today!';
+  telephone = '978-746-6012';
+  email = 'andrew@askplumbingandheating.com';
 }
---
+++ src/app/contactForm.ts
@@ -0,0 +1,14 @@
+
+export class Contact {
+
+  constructor(
+
+    private name: string,
+    private email: string,
+    private number: string,
+    private subject: string,
+    private comment: string,
+
+  ) { }
+
+}
