89555e683a42562d14b30f04f8e9ded9e2ad6c1d
rich-caceres <richard.caceres22590@outlook.com>
2022-07-31 10:53:48 -0400

    code cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { Contact } from './app.contactForm'
-import { FacebookPlugin } from './app.facebook'
 import { FacebookModule } from 'ngx-facebook'
 
 @NgModule({
   declarations: [
     AppComponent,
-    Contact,
-    FacebookPlugin
+    Contact
   ],
   imports: [
     BrowserModule,
